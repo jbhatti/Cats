0ab10ef5cc0a723eb989ef75f0c3d0c7f6e34752
Jaison Bhatti <jaison.bhatti@gmail.com>
2017-09-29 13:22:49 -0400

    add detail view controller

---
+++ CatsTemplateDemo/DetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  DetailViewController.h
+//  CatsTemplateDemo
+//
+//  Created by Jaison Bhatti on 2017-09-29.
+//  Copyright © 2017 Occasionally Cogent. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController
+
+@end
